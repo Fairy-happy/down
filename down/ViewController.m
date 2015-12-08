717f1b541e27a0834abfb90938ad2f09a342b0d1
Fairy-happy <fairy@fairy.local>
2015-12-08 15:33:48 +0800

    xiazai

--- down/ViewController.m
+++ down/ViewController.m
@@ -18,6 +18,50 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+- (IBAction)tap:(id)sender {
+
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"https://www.baidu.com"]];
+    NSURLSession *urlSession = [NSURLSession sharedSession];
+    NSURLSessionDataTask *dataTask = [urlSession dataTaskWithRequest:request
+                                                   completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                                                       NSLog(@"data > %@", data);
+                                                   }];
+    [dataTask resume];
+    //    [dataTask suspend];
+    //    [dataTask cancel];
+    
+    NSURLSessionDownloadTask *downloadTask = [urlSession downloadTaskWithRequest:request
+                                                               completionHandler:^(NSURL *location, NSURLResponse *response, NSError *error) {
+                                                                   NSString *documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+                                                                   NSURL *documentsDirectoryURL = [NSURL fileURLWithPath:documentsPath];
+                                                                   NSURL *newFileLocation = [documentsDirectoryURL URLByAppendingPathComponent:[[response URL] lastPathComponent]];
+                                                                   [[NSFileManager defaultManager] copyItemAtURL:location toURL:newFileLocation error:nil];
+                                                               }];
+    [downloadTask resume];
+    
+    NSURLSessionUploadTask *uploadTask = [urlSession uploadTaskWithRequest:request
+                                                                  fromData:nil
+                                                         completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                                                             
+                                                         }];
+    [uploadTask resume];   }
+
+-(void)post
+{
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:@"url地址"]];
+    [request setHTTPMethod:@"POST"];
+    [request setHTTPBody:[@"version=1.0.0&os_version=7.1.2&os_type=iOS" dataUsingEncoding:NSUTF8StringEncoding]];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request
+                                                completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                                                    
+                                                    NSLog(@"获取到的数据>%@", [[NSString alloc] initWithData:data
+                                                                                              encoding:NSUTF8StringEncoding]);
+                                                }];
+    [dataTask resume];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
